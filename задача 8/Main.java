24db999198873002786246640f892a484a067542
HezamALi <hezam1404@gmail.com>
2022-07-08 13:48:15 +0300

    Permutations

---
+++ задача 8/Main.java
@@ -0,0 +1,46 @@
+
+/*
+Given an array nums of distinct integers, return all the possible permutations.
+ You can return the answer in any order
+ */
+//Али Хезам 11-013
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Main {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        System.out.println(permute(nums));
+    }
+
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        helper(0, nums, result);
+        return result;
+    }
+
+    private static void helper(int start, int[] nums, List<List<Integer>> result) {
+        if (start == nums.length - 1) {
+            ArrayList<Integer> list = new ArrayList<>();
+            for (int num : nums) {
+                list.add(num);
+            }
+            result.add(list);
+            return;
+        }
+
+        for (int i = start; i < nums.length; i++) {
+            swap(nums, i, start);
+            helper(start + 1, nums, result);
+            swap(nums, i, start);
+        }
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
+
